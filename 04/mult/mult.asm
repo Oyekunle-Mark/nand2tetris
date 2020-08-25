10e68dbffef25a507fa2e54cbf23e5a8874e5553
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-25 19:07:40 +0100

    Multiplication program implementation

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    @2
+    M=0
+    @0
+    D=M
+    @END
+    D;JEQ
+    @1
+    D=M
+    @END
+    D;JEQ
+    @0
+    D=M
+    @2
+    M=D
+    @size
+    M=D
+(LOOP)
+    @1
+    M=M-1
+    D=M
+    @END
+    D;JEQ
+    @size
+    D=M
+    @2
+    M=M+D
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
