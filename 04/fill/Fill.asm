83fefecd5b08c01ae1b65f86cc7aba9dbfea1c3b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-31 20:42:42 +0100

    Turns screen black one way missing a row

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,25 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(START)
+    @24576
+    D=M     // set D to the keyboard map
+    @START
+    D;JEQ   // goto start of program if d is 0
+    @16384
+    D=A
+    @screenIndex
+    M=D
+(BLACK)
+    @24575
+    D=D-A   // check if all the screen memory map has been written to
+    @START
+    D;JEQ
+    @screenIndex
+    D=M + 1
+    A=D
+    M=-1    // turn it all black
+    @screenIndex
+    M=D
+    @BLACK
+    0;JMP
