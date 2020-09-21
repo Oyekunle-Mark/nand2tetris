97651f4ef284e67c83765eaea94be9d9a83c8d78
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-09-21 22:34:30 +0100

    Added helper functions for ggetters

--- 06/assembler/src/code.cpp
+++ 06/assembler/src/code.cpp
@@ -8,3 +8,15 @@ Code::Code()
         : destTable{std::make_unique<types::DestTable>()},
           compTable{std::make_unique<types::CompTable>()},
           jumpTable{std::make_unique<types::JumpTable>()} {}
+
+const std::string Code::dest(const std::string &key) const {
+    return destTable->table.at(key);
+}
+
+const std::string Code::comp(const std::string &key) const {
+    return compTable->table.at(key);
+}
+
+const std::string Code::jump(const std::string &key) const {
+    return jumpTable->table.at(key);
+}
