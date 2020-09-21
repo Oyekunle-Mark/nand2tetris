843b4b596b7941b55cfb482d9d0ed17ba9c776e0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-09-21 22:53:28 +0100

    The types header file

---
+++ 06/assembler/types.h
@@ -0,0 +1,81 @@
+//
+// Created by Oyekunle Oloyede on 21/09/2020.
+//
+
+#ifndef ASSEMBLER_TYPES_H
+#define ASSEMBLER_TYPES_H
+
+#include <map>
+#include <string>
+
+namespace types {
+    enum class Commandtype {
+        A_COMMAND,
+        C_COMMAND,
+        L_COMMAND
+    };
+
+    struct DestTable {
+        const std::map<std::string, std::string> table{
+                {"null", "000"},
+                {"M",    "001"},
+                {"D",    "010"},
+                {"MD",   "011"},
+                {"A",    "100"},
+                {"AM",   "101"},
+                {"AD",   "110"},
+                {"AMD",  "111"}
+        };
+    };
+
+    struct JumpTable {
+        const std::map<std::string, std::string> table{
+                {"null", "000"},
+                {"JGT",  "001"},
+                {"JEQ",  "010"},
+                {"JGE",  "011"},
+                {"JLT",  "100"},
+                {"JNE",  "101"},
+                {"JLE",  "110"},
+                {"JMP",  "111"}
+        };
+    };
+
+    struct CompTable {
+        const std::map<std::string, std::string> table{
+                // a is zero
+                {"0",   "0101010"},
+                {"1",   "0111111"},
+                {"-1",  "0111010"},
+                {"D",   "0001100"},
+                {"A",   "0110000"},
+                {"!D",  "0001101"},
+                {"!A",  "0110001"},
+                {"-D",  "0001111"},
+                {"-A",  "0110011"},
+                {"D+1", "0011111"},
+                {"A+1", "0110111"},
+                {"D-1", "0001110"},
+                {"A-1", "0110010"},
+                {"D+A", "0000010"},
+                {"D-A", "0010011"},
+                {"A-D", "0000111"},
+                {"D&A", "0000000"},
+                {"D|A", "0010101"},
+
+                // a is 1
+                {"M",   "1110000"},
+                {"!M",  "1110001"},
+                {"-M",  "1110011"},
+                {"M+1", "1110111"},
+                {"M-1", "1110010"},
+                {"D+M", "1000010"},
+                {"D-M", "1010011"},
+                {"M-D", "1000111"},
+                {"D&M", "1000000"},
+                {"D|M", "1010101"},
+        };
+    };
+}
+
+#endif //ASSEMBLER_TYPES_H
