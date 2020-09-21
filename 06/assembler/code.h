12e736d0c545f0e834e859b6bfa29cfdcdaa304f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-09-21 21:09:28 +0100

    Code class forwared declaration

---
+++ 06/assembler/code.h
@@ -0,0 +1,32 @@
+//
+// Created by Oyekunle Oloyede on 21/09/2020.
+//
+
+#ifndef ASSEMBLER_CODE_H
+#define ASSEMBLER_CODE_H
+
+#include <string>
+#include <memory>
+#include <map>
+
+class Code {
+public:
+    Code();
+
+    Code(const Code &) = delete;
+
+    Code &operator=(const Code &) = delete;
+
+    const std::string dest(const std::string &) const;
+
+    const std::string comp(const std::string &) const;
+
+    const std::string jump(const std::string &) const;
+
+private:
+    std::unique_ptr<std::map<std::string, std::string>> destTable{};
+    std::unique_ptr<std::map<std::string, std::string>> compTable{};
+    std::unique_ptr<std::map<std::string, std::string>> jumpTable{};
+};
+
+#endif //ASSEMBLER_CODE_H
