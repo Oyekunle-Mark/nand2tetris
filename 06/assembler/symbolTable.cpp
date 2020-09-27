7c38385d15002704046a1d2edf262b6226bbb4db
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-09-27 08:46:56 +0100

    Checks contains

--- 06/assembler/symbolTable.cpp
+++ 06/assembler/symbolTable.cpp
@@ -2,11 +2,15 @@
 // Created by Oyekunle Oloyede on 27/09/2020.
 //
 
-#include "SymbolTable.h"
+#include "symbolTable.h"
 
 SymbolTable::SymbolTable()
         : symTable{std::make_unique<types::SymTable>()} {}
 
 void SymbolTable::addEntry(const std::string &symbol, const std::string &address) {
     symTable->table.insert(std::pair<std::string, std::string>(symbol, address));
+}
+
+bool SymbolTable::contains(const std::string &symbol) const {
+    return symTable->table.count(symbol);
 }
